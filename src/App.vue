4eddde7423673558bd46231b68d84d6751917f05
Rewrite00 <747556795@qq.com>
2022-12-02 09:54:55 +0800

    change: app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { onLaunch } from '@dcloudio/uni-app';
 import { token } from '@/utils/request.js';
-import { getProvince, getYears } from '@/api';
 
 onLaunch(async () => {
   const { statusBarHeight } = uni.getSystemInfoSync();
@@ -11,23 +10,6 @@ onLaunch(async () => {
     uni.reLaunch({
       url: '/pages/login/login',
     });
-  } else {
-    const provinceData = await getProvince();
-    const yearsData = await getYears();
-
-    console.log('onLaunch', provinceData);
-
-    if (provinceData.code == 400) {
-      uni.clearStorageSync('province');
-      uni.clearStorageSync('years');
-      uni.clearStorageSync('token');
-      uni.reLaunch({
-        url: '/pages/login/login',
-      });
-    } else {
-      uni.setStorageSync('province', provinceData.data);
-      uni.setStorageSync('years', yearsData.data);
-    }
   }
 });
 </script>
